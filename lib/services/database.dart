23f78d6fed6bdc81944ac8ccbd1d57d13aa9cfc2
Ranvir <rsahota666@gmail.com>
2022-05-03 01:01:00 +0100

    created queries for matchmaking and images collections

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DatabaseService {
   late final String uid;
   //collection reference
 
   final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
+  final CollectionReference match = FirebaseFirestore.instance.collection('matchmaking');
+  final CollectionReference images = FirebaseFirestore.instance.collection('images');
 
   DatabaseService({required this.uid});
 
@@ -14,9 +17,22 @@ class DatabaseService {
     });
   }
 
-  Future addUserToQueue() async {
+  Future addUserToQueue(String username, Function onComplete) {
+    return match.doc("username").set(username);
   }
 
-  Future removeUserFromQueue() async {
+  // This would query the collection for game parameter
+  Future<List<QueryDocumentSnapshot<dynamic>> > lookingFor(String gameName) async {
+    final game = await match.where("game", isEqualTo: gameName).get();
+
+    return game.docs;
   }
+
+  //searching for randomly selected image by 'id'
+  Future getImage(int id) async {
+    final imagesList = await images.where("id", isEqualTo: id).get();
+    return imagesList.docs;
+  }
+
+  Future removeUserFromQueue() async {}
 }
