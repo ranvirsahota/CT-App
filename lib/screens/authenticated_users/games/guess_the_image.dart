54496c8c7de4485bd9ddb729d5073c35f8f18f11
Ranvir <rsahota666@gmail.com>
2022-05-04 21:06:57 +0100

    created stream to update listview when fetchMessages returs new message

--- lib/screens/authenticated_users/games/guess_the_image.dart
+++ lib/screens/authenticated_users/games/guess_the_image.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ct_app/models/game_search.dart';
+import 'package:ct_app/models/message.dart';
 import 'package:ct_app/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
@@ -31,44 +32,76 @@ class _GuessTheWordState extends State<GuessTheWord> {
 
   @override
   Widget build(BuildContext context) => Scaffold (
-    appBar: AppBar (
-      title: const Text('Guess the image'),
-    ),
-    body: Column (
-      children: [
-        Center(
-          child: CircularCountDownTimer (
-            duration: widget.timer,
-            initialDuration: 0,
-            controller: CountDownController(),
-            width: 30,
-            height: 30,
-            ringColor: Colors.grey[300]!,
-            ringGradient: null,
-            fillColor: Colors.purpleAccent[100]!,
-            fillGradient: null,
-            backgroundColor: Colors.purple[500],
-            backgroundGradient: null,
-            strokeWidth: 3.0,
-            strokeCap: StrokeCap.round,
-            textStyle: const TextStyle(
-                fontSize: 33.0, color: Colors.white, fontWeight: FontWeight.bold
+      appBar: AppBar (
+        title: const Text('Guess the image'),
+      ),
+      body: Column (
+        children: [
+          Center(
+            child: CircularCountDownTimer (
+              duration: widget.timer,
+              initialDuration: 0,
+              controller: CountDownController(),
+              width: 30,
+              height: 30,
+              ringColor: Colors.grey[300]!,
+              ringGradient: null,
+              fillColor: Colors.purpleAccent[100]!,
+              fillGradient: null,
+              backgroundColor: Colors.purple[500],
+              backgroundGradient: null,
+              strokeWidth: 3.0,
+              strokeCap: StrokeCap.round,
+              textStyle: const TextStyle(
+                  fontSize: 33.0, color: Colors.white, fontWeight: FontWeight.bold
+              ),
+              textFormat: CountdownTextFormat.S,
+              isReverse: true,
+              isReverseAnimation: true,
+              isTimerTextShown: true,
+              autoStart: true,
+              onStart: () {
+                debugPrint('Countdown Started');
+              },
+              onComplete: () {
+                debugPrint('Countdown Ended');
+              },
             ),
-            textFormat: CountdownTextFormat.S,
-            isReverse: true,
-            isReverseAnimation: true,
-            isTimerTextShown: true,
-            autoStart: true,
-            onStart: () {
-              debugPrint('Countdown Started');
-            },
-            onComplete: () {
-              debugPrint('Countdown Ended');
-            },
           ),
-        ),
-      ],
-    )
+          Expanded(
+            //listens to any changes made to the message
+            child: StreamBuilder<List<Message>> (
+              builder: (_, snapshot){
+                if(!snapshot.hasData ||  snapshot.hasError) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if(snapshot.hasError) {
+                  return Text("erro: ${snapshot.error}");
+                }
+                if(snapshot.requireData.isEmpty) {
+                  return Text("No message at the momennt");
+                }
+                final messages = snapshot.requireData;
+                return ListView.builder(
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card (
+                        elevation: 8,
+                        child: Padding (
+                          padding: const EdgeInsets.all(12),
+                          child: Text(messages[index].text),
+                        ),
+                      );
+                    }
+                );
+              },
+              stream: widget.databaseService!.fetchMessages(),
+            ),
+          )
+        ],
+      )
   );
 
   @override
