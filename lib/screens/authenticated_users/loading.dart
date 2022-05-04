a6c4d81dd68f344ca0b8ed44f9f25b535dad5439
Ranvir <rsahota666@gmail.com>
2022-05-04 15:41:26 +0100

    chages implmented have created the matchmaking system, issues will be stated in Github

--- lib/screens/authenticated_users/loading.dart
+++ lib/screens/authenticated_users/loading.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ct_app/models/game_search.dart';
-import 'package:ct_app/screens/authenticated_users/games/guess_the_word.dart';
+import 'package:ct_app/screens/authenticated_users/games/guess_the_image.dart';
 import 'package:flutter/material.dart';
 import 'package:ct_app/services/database.dart';
 
@@ -11,18 +11,47 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String? username = databaseService?.getUser()?.email;
+    int? suffix = username?.indexOf("@");
+    username = username?.substring(0, suffix ?? 0);
+    if(username != null) {
+      databaseService?.addUserToQueue("Guess the image", username);
+    }
+
     return  Scaffold(
-      //reading collections of matchmaking
-      body: FutureBuilder<List<QueryDocumentSnapshot<dynamic>> >(builder: (_, snapshot) {
-        final mappedData = snapshot.data?.map((e) => GameSearch.fromJson(e.data()) ).toList();
-        if (snapshot.hasData) {
-          return GuessTheWord(timer: 120, games: mappedData!,);
+      //listening for updates from firebase
+      body: StreamBuilder<QuerySnapshot<dynamic> >(builder: (_, snapshot) {
+        List<DocumentReference>? docRefs = [];
+        //maps data to GameSearch model
+        final mappedData = snapshot.data?.docs.map((e) {
+          docRefs.add(e.reference);
+          return GameSearch.fromJson(e.data());
+        }).toList();
+
+        //Checks for any existing data within snapshot
+        if (snapshot.hasData &&mappedData!.isNotEmpty) {
+          //want to ensure the user is paired before opening Guess the image screen
+          if (mappedData[0].username != username) {
+            //It waits for the entire widget to build, to prevent a crash
+            WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (setting) => GuessTheWord(docRefs, username, databaseService,
+                  timer: 120, games: mappedData)));
+            });
+          }
         }
-        return const Center(
-          child: CircularProgressIndicator(),
+
+        return Center(
+          child: Column (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Waiting for another user",),
+              SizedBox(height: 12,),
+              CircularProgressIndicator(),
+            ],
+          ),
         );
-       //value hard-coded for demo purposes
-      }, future: databaseService?.lookingFor("Guess the image"),)
+       //game: value hard-coded for demo purposes
+      }, stream: databaseService?.lookingForV2("Guess the image", username),)
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,31 +1,72 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ct_app/models/game_search.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class DatabaseService {
   late final String uid;
+
   //collection reference
 
   final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
   final CollectionReference match = FirebaseFirestore.instance.collection('matchmaking');
   final CollectionReference images = FirebaseFirestore.instance.collection('images');
+  final CollectionReference games = FirebaseFirestore.instance.collection('games');
+  final CollectionReference collectionMatch = FirebaseFirestore.instance.collection('match');
+  //look for way to get collection inside game
+  String? id;
 
   DatabaseService({required this.uid});
 
-  Future updateUserData(String username) async {
+  bool isGameAdded = false;
+
+  static String? gameId;
+
+  Future updateUserData({String? username, String? game_id}) async {
+    if (game_id != null) {
+      return await userCollection.doc(uid).update({"game_id": game_id});
+    }
     return await userCollection.doc(uid).set({
-      "username":username,
+      "username": username
     });
   }
 
-  Future addUserToQueue(String username, Function onComplete) {
-    return match.doc("username").set(username);
+  User? getUser() {
+    final user = FirebaseAuth.instance.currentUser;
+    return user;
   }
 
-  // This would query the collection for game parameter
-  Future<List<QueryDocumentSnapshot<dynamic>> > lookingFor(String gameName) async {
-    final game = await match.where("game", isEqualTo: gameName).get();
+  Future addUserToQueue(String? game, String username) async {
+    final newGame = await match.where("game", isEqualTo: game,)
+    //.where("waiting", isEqualTo: false)
+        .where("username", isNotEqualTo: username)
+        .get();
 
-    return game.docs;
+    final waitingGames = newGame
+        .docs.map((e)
+    => GameSearch.fromJson( e.data() as Map<String, dynamic>)).toList(); // Game is waiting. Get the ID of the game
+
+    if (waitingGames.isEmpty) {
+      final newMatch = match.doc();
+      await newMatch.set({"game": game, "username": username});
+      collectionMatch.doc(username).set({"updateId": newMatch.id});
+      updateUserData(game_id:  newMatch.id);
+    }else{
+      final storedId = await collectionMatch.doc(waitingGames[0]
+          .username).get();
+      if(storedId.exists) {
+        updateUserData(game_id: storedId.get("updateId"));
+        addGame( storedId.get("updateId"));
+      }
+      await match.doc(newGame.docs[0].id).update({"game": game, "username":"${waitingGames[0].username}#${username}"
+      });
+    }
+  }
+  //listening for events on matchmaking
+  Stream<QuerySnapshot<dynamic>> lookingForV2(String? gameName,
+      String? username) {
+    final game = match.where("game", isEqualTo: gameName,).where("username", isNotEqualTo: username).snapshots();
+    return game;
   }
 
   //searching for randomly selected image by 'id'
@@ -34,5 +75,38 @@ class DatabaseService {
     return imagesList.docs;
   }
 
-  Future removeUserFromQueue() async {}
+  void addGame(String matchId) {
+    final existingMatch = games.where("match_id", isEqualTo: matchId).get();
+    existingMatch.then((event) async {
+      if (event.docs.isEmpty) {
+        final msgDoc =  games.doc();
+        gameId = msgDoc.id;
+        await msgDoc.set({"match_id": matchId});
+      }
+    }, onError: (error) {
+      print("addGame_addGame => $error");
+    });
+  }
+
+
+    void removeUserFromQueue(List<DocumentReference> docRefs,
+        String username, String? gameName) async {
+      // Other user
+      final gameMatch = await match.where("game", isEqualTo: gameName).get();
+      final gameToDelete = gameMatch
+          .docs.map((e)
+      => GameSearch.fromJson( e.data() as Map<String, dynamic>)).toList();
+      if(gameToDelete.isNotEmpty) {
+        if (gameToDelete[0].username != username &&
+            (gameToDelete[0].username?.contains("#") ?? false)) {
+          match.doc(gameMatch.docs[0].id).delete();
+        }
+      }
+  }
+
+  void removeGame(){
+    if(gameId == null)return;
+    //games.doc(gameId).delete();
+  }
+
 }
