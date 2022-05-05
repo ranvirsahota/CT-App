ed4b08036e6a9395c7b79e36f1bd1fcdf4878fa9
Ranvir <rsahota666@gmail.com>
2022-05-05 22:05:29 +0100

    added new value created_at to store time message was created

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -2,13 +2,12 @@ class Message {
   final String text;
   final String text_from;
   final String messageId;
+  final dynamic created_at;
 
-
-  const Message ({required this.text, required this.text_from,
-    required this.messageId});
+  const Message ({required this.text, required this.text_from, required this.messageId, required this.created_at});
 
   factory Message.fromJson(Map<String, dynamic> json){
-    return Message(text: json["text"], text_from: json["text_from"], messageId: json["messageId"]);
+    return Message(text: json["text"], text_from: json["text_from"], messageId: json["messageId"], created_at: json["created_at"]);
   }
 
   //converting Message to map
@@ -17,6 +16,7 @@ class Message {
     json["text"] = text;
     json["text_from"] = text_from;
     json['messageId'] = messageId;
+    json["created_at"] = created_at;
     return json;
   }
 }
